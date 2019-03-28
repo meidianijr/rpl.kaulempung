0941330106bacfc6f810f0e5265be3719d43a438
ervinasiskawani <ervinasiskawani@gmail.com>
2019-03-28 10:46:10 +0700

    eighth commit

---
+++ app/src/main/java/com/sourcey/KauLempung/LupaPassword.java
@@ -0,0 +1,14 @@
+package com.sourcey.KauLempung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LupaPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lupa_password);
+
+    }
+}
